a2a1388a145b30ba4f23a022b7b03e5a6b956aca
90401 <904016613@qq.com>
2018-09-26 11:08:32 +0800

    hha,编写customer类，2018年9月26日11:08:01

---
+++ src/main/java/com/lcvc/tsg/mold/Customer.java
@@ -0,0 +1,72 @@
+package com.lcvc.tsg.mold;
+
+public class Customer {
+    private  int id;
+    private  String customer_name;//用户名字
+    private  String customer_passwod;//用户密码
+    private  String customer_head;//头像
+    private String customer_phone;//用户手机号
+    private  String customer_Email;//用户邮箱
+    private  boolean customer_sex;//用户性别
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public String getCustomer_passwod() {
+        return customer_passwod;
+    }
+
+    public void setCustomer_passwod(String customer_passwod) {
+        this.customer_passwod = customer_passwod;
+    }
+
+    public String getCustomer_head() {
+        return customer_head;
+    }
+
+    public void setCustomer_head(String customer_head) {
+        this.customer_head = customer_head;
+    }
+
+    public String getCustomer_phone() {
+        return customer_phone;
+    }
+
+    public void setCustomer_phone(String customer_phone) {
+        this.customer_phone = customer_phone;
+    }
+
+    public String getCustomer_Email() {
+        return customer_Email;
+    }
+
+    public void setCustomer_Email(String customer_Email) {
+        this.customer_Email = customer_Email;
+    }
+
+    public boolean isCustomer_sex() {
+        return customer_sex;
+    }
+
+    public void setCustomer_sex(boolean customer_sex) {
+        this.customer_sex = customer_sex;
+    }
+
+
+
+
+
+}
