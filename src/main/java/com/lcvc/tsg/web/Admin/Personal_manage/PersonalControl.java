565027a2055018de074fdcbce6e13b60ad30326a
Administrator <17425@qq.com>
2018-10-25 13:10:22 +0800

    根据ID修改管理 /Junji

--- src/main/java/com/lcvc/tsg/web/Admin/Personal_manage/PersonalControl.java
+++ src/main/java/com/lcvc/tsg/web/Admin/Personal_manage/PersonalControl.java
@@ -1,5 +1,6 @@
 package com.lcvc.tsg.web.Admin.Personal_manage;
 
+import com.lcvc.tsg.dao.AdminDao.AdminDao;
 import com.lcvc.tsg.model.Admin;
 import com.lcvc.tsg.servers.Admin.AdminBean;
 import com.lcvc.tsg.servers.Admin.PersonalBean;
@@ -25,6 +26,10 @@ import java.util.Map;
 public class PersonalControl {
     @Resource
     private PersonalBean personalBean;
+    @Resource
+    private AdminDao adminDao;
+    @Resource
+    private AdminBean adminBean;
     //----------------------------用户管理模块的控制层----------------------
     @RequestMapping(value = "/admin/AdminShow", method = RequestMethod.GET)
     public String AdminShow(HttpServletRequest request, Integer index) {
@@ -76,5 +81,39 @@ public class PersonalControl {
         }
         return map;
     }
+    //----------------------------------跳转修改管理员基本信息--------------------------
+    @RequestMapping(value = "/admin/goupdateubase", method = RequestMethod.GET)
+    public String updateAdmin(Integer id, HttpSession session) {
+        session.setAttribute("getAdmin",adminDao.getAdmin(id));
+        return "/admin/Manag/Adminupdate.jsp";
+    }
+    //----------------------------------修改管理基本信息--------------------------
+    @ResponseBody
+    @RequestMapping(value = "/admin/goupdateubase", method = RequestMethod.POST)
+    public Map<String, Object> updateAdmin(Admin admin, HttpSession session) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        Admin admin_id = (Admin) session.getAttribute("getAdmin");
+        admin.setId(admin_id.getId());
+        if (admin.getAdmin_nickname().equals(admin_id.getAdmin_nickname())) {
+            if (adminBean.updateAdmin(admin) > 0) {
+                session.setAttribute("getAdmin", adminBean.getAdmin(admin_id.getId()));
+                map.put("massage", 1);
+            } else {
+                map.put("massage", "修改失败！");
+            }
+        } else {
+            if (adminBean.AdminRename(admin.getAdmin_nickname()) > 0) {
+                map.put("massage", 2);
+            } else {
+                if (adminBean.updateAdmin(admin) > 0) {
+                    session.setAttribute("getAdmin", adminBean.getAdmin(admin_id.getId()));
+                    map.put("massage", 1);
 
+                } else {
+                    map.put("massage", "修改失败！");
+                }
+            }
+        }
+        return map;
+    }
 }
